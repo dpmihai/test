dfcbc3dbd6de465c24156b6e3f66080396ea27f0
Mihai <dpmihai@yahoo.com>
2013-10-14 13:53:11 +0300

    test vor ${val} in regex

--- src/regex/TestReplace.java
+++ src/regex/TestReplace.java
@@ -3,8 +3,12 @@ package regex;
 public class TestReplace {
 	
 	public static void main(String[] args) {
-		String s = "w234frT5";
-		s = s.replaceAll("\\D", "");
+//		String s = "w234frT5";
+//		s = s.replaceAll("\\D", "");
+//		System.out.println("s = " + s);
+		
+		String s = "SysInd > 100 ${val}";
+		s = s.replaceAll("\\$\\{val\\}", "435");
 		System.out.println("s = " + s);
 	}
 
